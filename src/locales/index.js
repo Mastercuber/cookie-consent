c138f849c84f6253c76e7b9f475f58a7f7c4ad6a
Mastercuber <armin@avensio.de>
2021-12-08 23:48:02 +0100

    renaming locale files to the appropriate ISO-639-1 codes

---
+++ src/locales/index.js
@@ -0,0 +1,67 @@
+import ar from './ar.json'
+import bg from './bg.json'
+import cs from './cs.json'
+import da from './da.json'
+import de from './de.json'
+import el from './el.json'
+import en from './en.json'
+import es from './es.json'
+import et from './et.json'
+import fi from './fi.json'
+import fr from './fr.json'
+import hi from './hi.json'
+import hr from './hr.json'
+import hu from './hu.json'
+import hy from './hy.json'
+import it from './it.json'
+import lb from './lb.json'
+import lt from './lt.json'
+import lv from './lv.json'
+import nl from './nl.json'
+import no from './no.json'
+import pl from './pl.json'
+import pt from './pt.json'
+import ro from './ro.json'
+import ru from './ru.json'
+import sk from './sk.json'
+import sl from './sl.json'
+import sq from './sq.json'
+import sv from './sv.json'
+import tk from './tk.json'
+import uk from './uk.json'
+import zh from './zh.json'
+
+export default {
+  ar,
+  sq,
+  hy,
+  bg,
+  zh,
+  cs,
+  de,
+  da,
+  et,
+  es,
+  fi,
+  fr,
+  en,
+  el,
+  hr,
+  hu,
+  hi,
+  it,
+  lt,
+  lb,
+  lv,
+  nl,
+  no,
+  pl,
+  pt,
+  ro,
+  ru,
+  sl,
+  sk,
+  sv,
+  tk,
+  uk
+}
