282f8ffc43c25a84c062c5ae33cb3383023a281c
Mastercuber <armin@avensio.de>
2021-12-08 23:54:24 +0100

    remove loading locale files (replaced by custom i18n blocks)

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import { createApp } from 'vue';
 import { createI18n } from 'vue-i18n';
-// @ts-ignore
-import messages from '@intlify/vite-plugin-vue-i18n/messages';
 import App from './App.vue';
 
 const app = createApp(App)
 const i18n = createI18n({
-    locale: 'en',
-    messages
+    locale: 'en'
 })
 app.use(i18n).mount('#app')
