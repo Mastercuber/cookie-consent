e5893f25b45ad890a4c5c41d8d0892da918dba9f
Mastercuber <armin@avensio.de>
2022-01-07 04:24:20 +0100

    Vite lib config

---
+++ vite.config.lib.ts
@@ -0,0 +1,53 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueI18n from "@intlify/vite-plugin-vue-i18n"
+import VueTypeImports from 'vite-plugin-vue-type-imports'
+import dts from 'vite-plugin-dts'
+
+import { resolve } from 'path'
+// @ts-ignore
+import autoprefixer from 'autoprefixer'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    VueTypeImports(),
+    vueI18n({
+      include: resolve(__dirname, 'src/locales/**'),
+      fullInstall: false
+    }),
+    dts()
+  ],
+  css: {
+    postcss: {
+      plugins: [
+          autoprefixer()
+      ]
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  build: {
+    target: 'esnext',
+    lib: {
+      entry: resolve(__dirname, 'src/components/CookieConsent.vue'),
+      name: 'CookieConsent',
+      formats: ['es', 'umd'],
+      fileName: (format) => `cookie-consent.${format}.js`
+    },
+    ssr: false,
+    minify: false,
+    rollupOptions: {
+      external: ['vue', 'vue-i18n'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+})
