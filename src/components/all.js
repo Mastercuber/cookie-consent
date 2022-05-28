433eaa4341de51af4be32865e8f0c2e9c8a1af46
Mastercuber <mr_w@posteo.de>
2022-05-28 20:22:44 +0200

    Added some tests and a vitest; Include Z in transforms to enable hardware acceleration; Add new library configuration, which includes the cookie consent and a smaller one with fewer languages included

---
+++ src/components/all.js
@@ -0,0 +1,7 @@
+import CookieConsent from './CookieConsent.vue'
+import CookieConsentFewerLanguages from './CookieConsentFewerLanguages.vue'
+
+export default {
+  CookieConsent,
+  CookieConsentFewerLanguages
+}
